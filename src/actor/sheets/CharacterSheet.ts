9d9a457783f1a25359992df784b563875b2f5dd9
Mezryss <mezryss@minalien.dev>
2023-05-03 20:45:34 -0700

    Confirm that new approach to sheet context properly maintains reactivity.

--- src/actor/sheets/CharacterSheet.ts
+++ src/actor/sheets/CharacterSheet.ts
@@ -1,12 +1,134 @@
 import { VueSheet } from '@/VueSheet';
+import InfinityItem from '@/item/InfinityItem';
+import TalentDataModel from '@/item/data/TalentDataModel';
+import InfinityActor from '../InfinityActor';
 import InfinityActorSheet from '../InfinityActorSheet';
 import CharacterSheetView from '../views/CharacterSheetView.vue';
+
+/**
+ * Function Callbacks
+ */
+type CharacterSheetActions = {
+	/**
+	 * (EXPERIMENTATION PURPOSES) Updates the rank of a talent.
+	 */
+	updateTalentRank: (id: string, multiplier?: number) => Promise<void>;
+};
+
+/**
+ * Vue context for Character sheets.
+ */
+export type CharacterSheetContext = {
+	/**
+	 * Collection of actions the sheet view can call.
+	 */
+	actions: CharacterSheetActions;
+
+	/**
+	 * A link to the document. This should not be used by the Vue sheets directly, but is required for the Editor component.
+	 *
+	 * @private
+	 */
+	document: InfinityActor;
+
+	/**
+	 * Whether or not the sheet is editable.
+	 */
+	editable: boolean;
+
+	/**
+	 * Character icon.
+	 */
+	img: string;
+
+	/**
+	 * Character name.
+	 */
+	name: string;
+
+	/**
+	 * Character talents.
+	 */
+	talents: {
+		/**
+		 * Document UUID
+		 */
+		id: string;
+
+		/**
+		 * Talent Name
+		 */
+		name: string;
+
+		/**
+		 * Talent Icon
+		 */
+		img: string;
+
+		/**
+		 * Talent System
+		 */
+		system: TalentDataModel;
+	}[];
+};
 
 /**
  * Player Character sheet controller.
  */
 export class CharacterSheet extends VueSheet(InfinityActorSheet) {
+	/**
+	 * Sheet action bindings.
+	 */
+	private actions: CharacterSheetActions = {
+		updateTalentRank: this.updateTalentRank.bind(this),
+	};
+
+	/**
+	 * Vue Component
+	 */
 	override get vueComponent() {
 		return CharacterSheetView;
+	}
+
+	/**
+	 * Vue Context
+	 */
+	override async getVueContext(): Promise<CharacterSheetContext> {
+		return {
+			actions: this.actions,
+			document: this.actor,
+			editable: this.isEditable,
+			img: this.actor.img,
+			name: this.actor.name,
+
+			talents: this.actor.items
+				.filter((i) => i.type === 'talent')
+				.map((talent) => ({
+					id: talent.id,
+					name: talent.name,
+					img: talent.img,
+					system: talent.system,
+				})),
+		};
+	}
+
+	/**
+	 * (EXPERIMENTATION PURPOSES) Updates the rank of a talent.
+	 */
+	async updateTalentRank(id: string, multiplier: number = 1) {
+		const item = this.actor.items.find((i) => i.id === id) as
+			| InfinityItem<TalentDataModel>
+			| undefined;
+		if (!item || item.type !== 'talent' || !item.system.isRanked) {
+			return;
+		}
+
+		await item.update({
+			'system.rank.current': Math.clamped(
+				item.system.rank.current + multiplier,
+				1,
+				item.system.rank.max,
+			),
+		});
 	}
 }
--- src/actor/views/CharacterSheetView.vue
+++ src/actor/views/CharacterSheetView.vue
@@ -1,11 +1,37 @@
-<!--
-	Player Character sheet
--->
-
 <script lang="ts" setup>
+import { RootContext } from '@/VueSheet';
 import InfinitySheet from '@/components/InfinitySheet.vue';
+import { computed, inject } from 'vue';
+import { CharacterSheetContext } from '../sheets/CharacterSheet';
+
+const context = inject<CharacterSheetContext>(RootContext)!;
+
+const actions = computed(() => context.actions);
+const talents = computed(() => context.talents);
 </script>
 
 <template>
-	<InfinitySheet>Character Sheet</InfinitySheet>
+	<InfinitySheet class="flex flex-col">
+		<h1 class="w-full">Character Sheet</h1>
+		<h3 class="w-full">Talents</h3>
+		<div v-for="talent in talents" :key="talent.id">
+			<div class="flex gap-2 text-base">
+				<strong>{{ talent.name }}</strong>
+
+				<div
+					v-if="talent.system.isRanked"
+					class="flex w-full whitespace-nowrap gap-1 items-center"
+				>
+					<a class="text-lg" @click="actions.updateTalentRank(talent.id, -1)"
+						>-</a
+					>
+					<span class="whitespace-nowrap"
+						>{{ talent.system.rank.current }} /
+						{{ talent.system.rank.max }}</span
+					>
+					<a class="text-lg" @click="actions.updateTalentRank(talent.id)">+</a>
+				</div>
+			</div>
+		</div>
+	</InfinitySheet>
 </template>
