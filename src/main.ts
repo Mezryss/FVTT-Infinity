18cb694abd564b09d984edb19ee70cc65c90a453
Mezryss <mezryss@minalien.dev>
2025-04-28 16:20:08 -0700

    Text Enricher: Combat Die

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+import { register as registerTextEnrichers } from '@/text-enrichers';
+
 import '@/styles/main.css';
 
 Hooks.once('init', () => {
 	console.log('Infinity | Init');
+
+	registerTextEnrichers();
+
 	console.log('Infinity | Init Complete');
 });
---
+++ src/text-enrichers.ts
@@ -0,0 +1,14 @@
+/**
+ * Register all custom text enrichers.
+ */
+export function register() {
+	CONFIG.TextEditor.enrichers.push({
+		pattern: /@(N|combat(-die)?)/gi,
+		async enricher(_match, _options) {
+			const span = document.createElement('span');
+			span.classList.add('icon-combat-die')
+			span.innerText = 'N';
+			return span;
+		}
+	});
+}
